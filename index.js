00b3c293fdb86cdf504c026e79a63ac95e8adf86
Fanta3er-F3 <133674424+Fanta3er-F3@users.noreply.github.com>
2024-01-27 23:59:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Playing Real Rust"];
 
 
 let currentIndex = 0;
